c5639d8d2e7b10a90ce8516d5cd2df85bb74f74d
ezgi02 <104259592+ezgi02@users.noreply.github.com>
2023-07-19 02:19:31 +0300

    Add files via upload

--- UserSignupPage.js
+++ UserSignupPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios from "axios";
 class UserSignupPage extends React.Component {
     state={
         username:null,
@@ -13,27 +13,38 @@ class UserSignupPage extends React.Component {
             [name]:value
         })
     }
+    onClickSignup=event=>{
+        event.preventDefault();
+        const body={
+            username:this.state.username,
+            surname:this.state.surname,
+            tc:this.state.tc,
+            phone:this.state.phone
+        }
+        axios.post("/api/1.0/users",body)
+    }
     render() {
         return (
             <form>
                <h1>Car Sigortama Hoşgeldiniz</h1> 
                <div>
                     <label>Kullanici Adi:</label>
-                    < input name="username"/>
+                    < input name="username" onChange={this.onChange}/>
                 </div>
                 <div>
                     <label>Kullanici Soyadi:</label>
-                    <input name="surname"/>
+                    <input name="surname" onChange={this.onChange}/>
                 </div>
                 <div>
                     <label>Ruhsat Sahibi TC:</label>
-                    <input name="tc"/>
+                    <input name="tc"onChange={this.onChange}/>
                 </div>
                 <div>
                     <label>Cep Telefonu:</label>
-                    <input name="phone"/>
+                    <input name="phone"onChange={this.onChange}/>
                 </div>
-                <button>Kargo Sigortasi Teklifi Al</button>
+                
+                <button onClick={this.onClickSignup}>Kargo Sigortasi Teklifi Al</button>
             </form>
             
 
